6496d0d6be831bcaee95fd36b3596fd81f367861
ChloeRevere <70986538+ChloeRevere@users.noreply.github.com>
2020-11-10 19:39:52 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,47 @@
+var ball;
+var database;
+var position;
+
+var ballPosition
+function setup(){
+    database = firebase.database();
+    createCanvas(500,500);
+    ball = createSprite(250,250,10,10);
+    ball.shapeColor = "red";
+    ballPosition = database.ref('car/position')
+    ballPosition.on("value",readPosition, showerror)
+
+}
+
+function draw(){
+    background("white");
+    if(keyDown(LEFT_ARROW)){
+        changePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        changePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        changePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        changePosition(0,+1);
+    }
+    drawSprites();
+}
+
+function readPosition(data){
+    position = data.val();
+    ball.x = position.x; ball.y = position.y
+
+}
+
+function showerror(){
+    console.log("error in the database")
+}
+
+function changePosition(x,y){
+   database.ref('car/position').set({
+       x:position.x+x, y:position.y+y
+   })
+}
